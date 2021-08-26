b7f14d9c3e0d9f3e5833d1b7d6ff7acfae79356a
juneandgreen <june_01@qq.com>
2021-08-26 14:33:30 +0800

    chore: use umd output for dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
         library: '[name]',
-        libraryTarget: 'window',
+        libraryTarget: 'umd',
     },
     module: {    
         loaders: [{    
